3493021c2cc21a1143ca737aac0edc2d7f9a3772
Ersin Erdal <ersin.erdal@elastic.co>
2024-09-16 12:50:56 +0200

    add new resources

--- src/indexers/metrics/config.ts
+++ src/indexers/metrics/config.ts
@@ -1,6 +1,13 @@
 import { MetricGeneratorConfig } from '../../generators/types';
 
 export const config: MetricGeneratorConfig = {
-  interval: 1000,
-  docs: ['cpu-001.json', 'cpu-002.json', 'cpu-003.json', 'memory-001.json'],
+  interval: 5000,
+  docs: [
+    'cpu-001.json',
+    'cpu-002.json',
+    'cpu-003.json',
+    'cpu-004.json',
+    'cpu-005.json',
+    'cpu-006.json',
+    'memory-001.json']
 };
